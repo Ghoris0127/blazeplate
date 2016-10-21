91c00f016639cc57cedf645e4c94ae2d3065ff11
WouterLangendoen <wouterlangendoen@hotmail.nl>
2016-10-22 00:23:09 +0200

    Add files via upload
    Hier heb je alvast de aan en uit knop. Ik heb de button op plaats 7 gezet en LED ff op 12 ik weet niet welke jij daarvoor gebruikt, maar dit was gewoon ff om het te testen of hij het deed via mijn breadboard. De kinderslot krijg je morgen, daar moet ik nog ff wat extra tijd aan besteden. Werkt namelijk nog niet helemaal

---
+++ arduino/Aanuitknop.ino
@@ -0,0 +1,25 @@
+int LED = 12;
+int BUTTON = 7;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON,INPUT);
+}
+
+void loop()
+{ 
+  if(digitalRead(BUTTON) == HIGH)
+  {
+  digitalWrite(LED,HIGH);
+  }else
+  
+    digitalWrite(LED,LOW);
+    
+  {
+  
+  digitalWrite(LED,LOW);
+  }
+  }
+
+
